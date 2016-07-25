393c1b0c72ae574a93c623de96a54b97d321883f
James Jester <elmojester@gmail.com>
2016-07-25 03:14:44 -0500

    Changed script to be mobile responsive.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 
 
-// (function hideOnPageLoad(){
-//     // $('#me1').hide();
-//     // $('#me2').hide();
-//     // $('#me3').hide();
-//     // $('#me4').hide();
-//     // $('#me5').hide();
-//     // $('#me6').hide();
-//     $(".fact-list").toggle();
-// }());
-
-
-var options = [
-    {selector: '.fact-list', offset: 300, callback: function(){
-        Materialize.showStaggeredList(".fact-list");
-    } }
-  ];
+if(window.innerWidth > 600){
+    console.log(window.innerWidth);
+    
+    var options = [
+                    {selector: '.fact-list', 
+                     offset: 300, 
+                     callback: function(){
+                     Materialize.showStaggeredList(".fact-list");
+                     } 
+                    }
+                  ];
+                    
   Materialize.scrollFire(options);
+}
+else{
+    console.log(window.innerWidth);
+   var options = [
+                   {selector: '.fact-list', 
+                    offset: 300, 
+                    callback: function() {
+                    Materialize.fadeInImage($('#numOne'));
+                    Materialize.fadeInImage($('#numTwo'));
+                    Materialize.fadeInImage($('#numThree'));
+                    Materialize.fadeInImage($('#numFour'));
+                    Materialize.fadeInImage($('#numFive'));
+                    Materialize.fadeInImage($('#numSix'));
+                    } 
+                   },
+                 ];
+  Materialize.scrollFire(options);
+}
+  
+  
+  
